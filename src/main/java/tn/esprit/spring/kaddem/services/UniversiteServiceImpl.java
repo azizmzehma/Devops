ce3517412d7d89643ffb77586c842298e2485ad4
azizmzehma <91553397+azizmzehma@users.noreply.github.com>
2024-11-06 16:06:51 +0100

    logs

--- src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring.kaddem.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.kaddem.entities.Departement;
@@ -11,43 +13,88 @@ import java.util.List;
 import java.util.Set;
 
 @Service
-public class UniversiteServiceImpl implements IUniversiteService{
-@Autowired
+public class UniversiteServiceImpl implements IUniversiteService {
+
+    private static final Logger logger = LogManager.getLogger(UniversiteServiceImpl.class);
+
+    @Autowired
     UniversiteRepository universiteRepository;
-@Autowired
+
+    @Autowired
     DepartementRepository departementRepository;
+
     public UniversiteServiceImpl() {
-        // TODO Auto-generated constructor stub
-    }
-  public   List<Universite> retrieveAllUniversites(){
-return (List<Universite>) universiteRepository.findAll();
+        logger.debug("UniversiteServiceImpl instantiated.");
     }
 
- public    Universite addUniversite (Universite  u){
-return  (universiteRepository.save(u));
+    public List<Universite> retrieveAllUniversites() {
+        logger.debug("Retrieving all universities.");
+        return (List<Universite>) universiteRepository.findAll();
     }
 
- public    Universite updateUniversite (Universite  u){
-     return  (universiteRepository.save(u));
+    public Universite addUniversite(Universite u) {
+        try {
+            logger.debug("Adding university: {}", u);
+            return universiteRepository.save(u);
+        } catch (Exception e) {
+            logger.error("Error adding university: ", e);
+            return null;
+        }
     }
 
-  public Universite retrieveUniversite (Integer idUniversite){
-Universite u = universiteRepository.findById(idUniversite).get();
-return  u;
-    }
-    public  void deleteUniversite(Integer idUniversite){
-        universiteRepository.delete(retrieveUniversite(idUniversite));
-    }
-
-    public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement){
-        Universite u= universiteRepository.findById(idUniversite).orElse(null);
-        Departement d= departementRepository.findById(idDepartement).orElse(null);
-        u.getDepartements().add(d);
-        universiteRepository.save(u);
+    public Universite updateUniversite(Universite u) {
+        try {
+            logger.debug("Updating university: {}", u);
+            return universiteRepository.save(u);
+        } catch (Exception e) {
+            logger.error("Error updating university: ", e);
+            return null;
+        }
     }
 
-    public Set<Departement> retrieveDepartementsByUniversite(Integer idUniversite){
-Universite u=universiteRepository.findById(idUniversite).orElse(null);
-return u.getDepartements();
+    public Universite retrieveUniversite(Integer idUniversite) {
+        logger.debug("Retrieving university with ID: {}", idUniversite);
+        return universiteRepository.findById(idUniversite)
+                .orElseThrow(() -> {
+                    logger.error("University with ID {} not found", idUniversite);
+                    return new RuntimeException("University not found");
+                });
+    }
+
+    public void deleteUniversite(Integer idUniversite) {
+        try {
+            logger.debug("Deleting university with ID: {}", idUniversite);
+            universiteRepository.delete(retrieveUniversite(idUniversite));
+        } catch (Exception e) {
+            logger.error("Error deleting university with ID: {}", idUniversite, e);
+        }
+    }
+
+    public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement) {
+        try {
+            logger.debug("Assigning department with ID {} to university with ID {}", idDepartement, idUniversite);
+            Universite u = universiteRepository.findById(idUniversite).orElse(null);
+            Departement d = departementRepository.findById(idDepartement).orElse(null);
+            if (u != null && d != null) {
+                u.getDepartements().add(d);
+                universiteRepository.save(u);
+                logger.debug("Department assigned successfully.");
+            } else {
+                logger.error("University or Department not found.");
+            }
+        } catch (Exception e) {
+            logger.error("Error assigning department to university: ", e);
+        }
+    }
+
+    public Set<Departement> retrieveDepartementsByUniversite(Integer idUniversite) {
+        logger.debug("Retrieving departments for university with ID: {}", idUniversite);
+        Universite u = universiteRepository.findById(idUniversite).orElse(null);
+        if (u != null) {
+            return u.getDepartements();
+        } else {
+            logger.error("University with ID {} not found.", idUniversite);
+            return null;
+        }
     }
 }
